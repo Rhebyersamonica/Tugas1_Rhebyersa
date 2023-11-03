3b55f4337dcc0cf64c591ba9167c37f9281c8db4
Rhebyersa <rhebyersa@gmail.com>
2023-11-03 21:40:12 +0700

    first commit

---
+++ src/Task1.java
@@ -0,0 +1,13 @@
+
+public class Task1{
+    public static void main(String[] args) {
+        for (int i = 1;i<=100;i++){
+            if (i<=9){
+                System.out.println(i);
+            } else {
+                System.out.println("Rheby Ersa Monica");
+            }
+        }
+    }
+
+}
